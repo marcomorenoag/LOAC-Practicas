ee64bad6405dea9ea937c4c4533dcdf162eb7fbb
Marco Antonio Moreno <marcomorenoag@outlook.com>
2019-11-23 13:11:52 -0600

    Proyecto final: nuevos comentarios y planteamiento de nueva idea de proyecto

--- Proyecto_RISC/memoria_inst.vhd
+++ Proyecto_RISC/memoria_inst.vhd
@@ -16,30 +16,33 @@ architecture Behavioral of memoria_inst is
 	
 	begin
 
-		-- Inicializa ACCA = 0, MEM_0 = 0, ACCB = 1
-		memoria(0) <= x"00860000"; -- LDAA 00
-		memoria(1) <= x"00B70004"; -- STAA #0004 (SUMA)
-		memoria(2) <= x"00C60001"; -- LDAB 01
-		-- Número limite
-		memoria(3) <= x"00860005"; -- LDAA 05 (n) --- JUMP AQUI
-		-- IF  ACCB > limite THEN RESET 
-		memoria(4) <= x"00110000"; -- CBA (ACCA - ACCB) Actualizar banderas 
-		memoria(5) <= x"00270013"; -- JB #0013 si se cumple salta al reset (x"13"=19)
+		-- Inicio: ACCA = 0, MEM_0 = 0, ACCB = 1
+		memoria(0) <= x"00860000"; -- LDAA #0000 ; (ACCA) <- 0x0000
+		memoria(1) <= x"00B70004"; -- STAA #0004 ; (mem[4]) <- (ACCA)
+		memoria(2) <= x"00C60001"; -- LDAB #0001 ; (ACCB) <- 0x0001
+		
+		-- límite = 5
+		memoria(3) <= x"00860005"; -- LDAA #0005 ; (ACCA) <- 0x0005
+		
+		-- IF (ACCB >= límite) THEN: reset 
+		memoria(4) <= x"00110000"; -- CBA ; (ACCA) - (ACCB) ; update flags (N, Z, V, C)
+		memoria(5) <= x"00270013"; -- JZ #0013 ; IF (Z == 1): PC <- (0x0013 = 0d0019)
 		memoria(6) <= x"00010000"; -- NOP
 		memoria(7) <= x"00010000"; -- NOP
 		memoria(8) <= x"00010000"; -- NOP
-		-- ELSE: A + B, B++
-		memoria(9) <= x"00960004";  -- LDAA #0004 acceso directo a memoria
-		memoria(10) <= x"001B0000"; -- ABA
-		memoria(11) <= x"005c0000"; -- INCB
-		memoria(12) <= x"00B70004"; -- STAA #0004 guarda suma
-		memoria(13) <= x"007E0003"; -- JMP al x"03"
+		
+		-- ELSE: (ACCA + ACCB; ACCB <- ACBB + 1)
+		memoria(9) <= x"00960004";  -- LDAA $#0004 ; (ACCA) <- (mem[4])
+		memoria(10) <= x"001B0000"; -- ABA ; (ACCA) <- (ACCA) + (ACCB)
+		memoria(11) <= x"005c0000"; -- INCB ; (ACCB) <- (ACCB) + 1
+		memoria(12) <= x"00B70004"; -- STAA #0004 ; (mem[4]) <- (ACCA)
+		memoria(13) <= x"007E0003"; -- JMP #0003 ; PC <- (0x0003)
 		memoria(14) <= x"00010000"; -- NOP
 		memoria(15) <= x"00010000"; -- NOP
 		memoria(16) <= x"00010000"; -- NOP
 
 		-- Subrutina de reset (fin de programa)
-		memoria(19) <= x"007E0000"; -- JMP al x"0"
+		memoria(19) <= x"007E0000"; -- JMP #0000 ; PC <- (0x0000)
 		memoria(20) <= x"00010000"; -- NOP
 		memoria(21) <= x"00010000"; -- NOP
 		memoria(22) <= x"00010000"; -- NOP
